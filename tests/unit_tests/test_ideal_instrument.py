2f7c17f4457a003a98ec6f6a72184aa0ba176c3d
Adam J. Jackson <a.j.jackson@physics.org>
2026-01-13 09:32:57 +0000

    Refactor Ideal instrument tests
    Parametrise a general function instead of creating a data function
    per-test.

--- tests/unit_tests/test_ideal_instrument.py
+++ tests/unit_tests/test_ideal_instrument.py
@@ -1,114 +1,92 @@
+from collections import ChainMap
+from enum import StrEnum
 from pathlib import Path
 
 import numpy as np
 from numpy.testing import assert_allclose
+import pytest
 
 from resolution_functions.instrument import Instrument
 
 
 DATA_PATH = Path(__file__).parent / 'data' / 'ideal'
 
-def _get_boxcar_kernel():
-    points = np.array([[1.], [2.]])
-    mesh = np.linspace(-5, 5, 11)
+
+TEST_CASES = {
+    'boxcar': {
+        'default': {'points': np.array([[1.], [2.]]),
+                    'mesh': np.linspace(-5, 5, 11),
+                    'kwargs': {'width': 3.}},
+        'peak': {'points': np.array([[0.], [2.]])},
+        'broaden': {'mesh': np.linspace(0, 5, 11),  # i.e. each bin is 0.5
+                    'points': np.linspace(0, 5, 11)[:, None],
+                    'data': np.array([0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0], dtype=float),
+                    'kwargs': {'width': 1.}},  # i.e. pulse area = 1}
+    },
+    'triangle': {
+        'default': {
+            'points': np.array([[3.], [7.]]),                
+            'mesh': np.arange(0., 10., 1.),
+            'data': np.array([0.5, 2.]),
+            'kwargs': {'fwhm': 2.}
+        },
+        'kernel': {
+            'mesh': np.arange(-5, 5, 1.)
+        }
+    }
+}
+
+
+class Feature(StrEnum):
+    KERNEL = 'kernel'
+    PEAK = 'peak'
+    BROADEN = 'broaden'
+
+
+def _get_data(name: str, feature: Feature):
+    params = ChainMap(TEST_CASES[name].get(feature, {}),
+                      TEST_CASES[name].get('default', {}))
     instrument = Instrument.from_default('IDEAL')
-    model = instrument.get_resolution_function('boxcar', width=3.)
+    model = instrument.get_resolution_function(name, **params.get('kwargs', {}))
 
-    return model.get_kernel(points, mesh), mesh
+    match feature:
+        case Feature.KERNEL:
+            result = model.get_kernel(params['points'], params['mesh'])
+        case Feature.PEAK:
+            result = model.get_peak(params['points'], params['mesh'])
+        case Feature.BROADEN:
+            result = model.broaden(params['points'], params['data'], params['mesh'])
+        case _:
+            raise ValueError()
 
-def test_boxcar_kernel():
-    result, _ = _get_boxcar_kernel()
-    assert_allclose(result, np.load(DATA_PATH / '_get_boxcar_kernel.npy'))
+    return result, params['mesh']
 
-def _get_boxcar_peak():
-    points = np.arange(0, 4, 2)[:, None]
-    mesh = np.linspace(-5, 5, 11)
-    instrument = Instrument.from_default('IDEAL')
-    model = instrument.get_resolution_function('boxcar', width=3.)
-
-    return model.get_peak(points, mesh), mesh
-
-def test_boxcar_peak():
-    result, _ = _get_boxcar_peak()
-    assert_allclose(result, np.load(DATA_PATH / '_get_boxcar_peak.npy'))
-
-def _get_boxcar_broaden():
-    mesh = np.linspace(0, 5, 11)  # i.e. each bin is 0.5
-    points = mesh[:, None]
-    data = [0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0]  # i.e. pulse area = 1
-
-    instrument = Instrument.from_default('IDEAL')
-    model = instrument.get_resolution_function('boxcar', width=1.)
-
-    return model.broaden(points, data, mesh), mesh
-
-def test_boxcar_broaden():
-    result, _ = _get_boxcar_broaden()
-    assert_allclose(result, np.load(DATA_PATH / '_get_boxcar_broaden.npy'))
-
-def _get_triangle_kernel():
-    points = np.array([[3.], [7.]])
-    mesh = np.arange(-5., 5., 1.)
-    instrument = Instrument.from_default('IDEAL')
-    model = instrument.get_resolution_function('triangle', fwhm=2.)
-
-    return model.get_kernel(points, mesh), mesh
-
-def test_triangle_kernel():
-    result, _ = _get_triangle_kernel()
-    assert_allclose(result, np.load(DATA_PATH / '_get_triangle_kernel.npy'))
-
-
-def _get_triangle_peak():
-    points = np.array([[3.], [7.]])
-    mesh = np.arange(0., 10., 1.)
-
-    instrument = Instrument.from_default('IDEAL')
-    model = instrument.get_resolution_function('triangle', fwhm=2.)
-
-    return model.get_peak(points, mesh), mesh
-
-def test_triangle_peak():
-    result, _ = _get_triangle_peak()
-    assert_allclose(result, np.load(DATA_PATH / '_get_triangle_peak.npy'))
-
-def _get_triangle_broaden():
-    points = np.array([[3.], [7.]])
-    mesh = np.arange(0., 10., 1.)
-    data = [0.5, 2.]
-    instrument = Instrument.from_default('IDEAL')
-    model = instrument.get_resolution_function('triangle', fwhm=2.)
-
-    return model.broaden(points, data, mesh), mesh
-
-def test_triangle_broaden():
-    result, _ = _get_triangle_broaden()
-    assert_allclose(result, np.load(DATA_PATH / '_get_triangle_broaden.npy'))
-
-
-_GET_DATA_FUNCTIONS = [
-    _get_boxcar_kernel,
-    _get_boxcar_peak,
-    _get_boxcar_broaden,
-    _get_triangle_kernel,
-    _get_triangle_peak,
-    _get_triangle_broaden,
+test_specs = [
+    ("boxcar", Feature.KERNEL),
+    ("boxcar", Feature.PEAK),
+    ("boxcar", Feature.BROADEN),
+    ("triangle", Feature.KERNEL),
+    ("triangle", Feature.PEAK),
+    ("triangle", Feature.BROADEN),
 ]
 
+@pytest.mark.parametrize("name,feature", test_specs)
+def test_ideal_model(name: str, feature: Feature):
+    result, _ = _get_data(name, feature)
+    assert_allclose(result, np.load(DATA_PATH / f'_get_{name}_{feature}.npy'))
 
 def generate_data():
     import matplotlib
-    matplotlib.use('TkAgg')
+    matplotlib.use('AGG')
     import matplotlib.pyplot as plt
 
     DATA_PATH.mkdir(exist_ok=True)
 
-    for func in _GET_DATA_FUNCTIONS:
-        name = str(func.__name__)
-        print(name)
+    for name, feature in test_specs:
+        print(f'Generating test data: {name}, {feature}')
 
-        result, mesh = func()
-        np.save(DATA_PATH / f'{name}.npy', result)
+        result, mesh = _get_data(name, feature)
+        np.save(DATA_PATH / f'_get_{name}_{feature}.npy', result)
 
         fig, ax = plt.subplots()
         if len(np.shape(result)) == 1:
@@ -117,7 +95,7 @@ def generate_data():
             for row in result:
                 ax.plot(mesh, row)
 
-        fig.savefig(DATA_PATH / f'{name}.png')
+        fig.savefig(DATA_PATH / f'_get_{name}_{feature}.png')
         plt.close(fig)
 
 
